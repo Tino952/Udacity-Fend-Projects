82744949cfee22d8e38ae8859593081454177e06
Tino Leitzmann <cmleitzmann@gmail.com>
2021-07-06 15:35:26 +0200

    final changes .. was a little lazy with git this time ..

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,60 +1,25 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
-*/
+/* ===================================
 
-/**
- * Define Global Variables
- *
-*/
+Calling all functions
 
+=================================== */
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
-*/
-
-// build the nav
-
-
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- *
-*/
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+/* calling our navigation initializer function. This function in turn first
+calls the addSection function to apoend additional sections in the DOM. Finally,
+the function underlineExtend is called which adds additional functionality to
+each section in the DOM. Note that this function is called with a timeout delay.
+The reason for this is because the user input triggered by the addSection function
+would otherwise interupt repaint & reflow of our page. You can test this out by
+setting the timeout to zero. */
+setTimeout(navInit, 1000);
+/* function which calls activeClass() which adjusts border box on current
+section in viewport. A scroll delay for large screen sizes creates a smoother
+functionality */
+activeClassScrollStop();
+/* adding a scroll to top functionality to chevron */
+chevronScrollToTop();
+/* making chevron rotate when scrolled to top of viewport */
+rotateChevron();
 
 /* ===================================
 
@@ -62,115 +27,72 @@ Building navigation menu
 
 =================================== */
 
-/* Make sure to add comments */
-
 function navInit() {
 
-  addSections()
-
+  /* Running function to add new sections to page */
+  addSections();
   const mySections = document.getElementsByTagName("section");
-
-  const myNavLarge = document.createElement("ul");
-
-  const myNavSmall = document.createElement("ul");
-
-  myNavLarge.classList.add("nav");
-
-  myNavSmall.classList.add("nav");
-
-  const myNavSmallDiv1 = document.createElement("div");
-
-  const myNavSmallDiv2 = document.createElement("div");
-
-  myNavSmallDiv1.classList.add("nav__container");
-
-  myNavSmallDiv2.classList.add("nav__container");
-
-  myNavSmall.appendChild(myNavSmallDiv1);
-
-  myNavSmall.appendChild(myNavSmallDiv2);
-
-  for (let section of mySections) {
-
-    const navItem = section.dataset.nav;
-
-    const node = document.createElement("li");
-
-    const link = document.createElement("a");
-
-    link.classList.add("nav__item__link");
-
-    node.appendChild(link);
-
-    link.textContent = navItem;
-
-    node.classList.add("nav__item");
-
-    const nodeClone = node.cloneNode(true);
-
-    /* even section numbers go in the right flex column of the nav,
-    while odd section numbers go in the left column */
-
-    if (navItem.split("-")[1] % 2 == 0) {
-      myNavSmallDiv2.appendChild(node);
-      }
-    else {
-      myNavSmallDiv1.appendChild(node);
-      }
-
-    /* for large screens the nav menu is just one container */
-
-    myNavLarge.appendChild(nodeClone);
-    }
-
-    return {
-      myNavLarge,
-      myNavSmall
-    }
-  }
-
-/* Creating a function to change navigation styles based
-on screen size */
-
-let a = navInit();
-let myNavLarge = a.myNavLarge;
-let myNavSmall = a.myNavLarge;
-
-
-
-function mediaWidth(myWindow) {
   const myNav = document.querySelector("nav");
-  myNav.appendChild(myNavSmall);
-  if (myWindow.matches) {
-    myNav.replaceChild(myNavLarge, myNavSmall)
+  /* initaliazing a new unordered list */
+  const navUl = document.createElement("ul");
+  /* adding an existing css class from layout.css */
+  navUl.classList.add("nav");
+  /* looping through all sections in page */
+  for (let section of mySections) {
+    /* retrieving section number from the dataset attribute */
+    const navItem = section.dataset.nav;
+    /* creating a list element */
+    const node = document.createElement("li");
+    /* adding section name */
+    node.textContent = navItem;
+    /* adding an existing css class from layout.css */
+    node.classList.add("nav__item");
+    /* adding an event listener to nav items to scroll into view when
+    clicked */
+    node.addEventListener("click", () =>
+    /* if window is greater than 400px then scroll to center,
+    otherwise for smaller screens scroll to top of section */
+    { if (window.innerWidth > 400) {
+      section.scrollIntoView({block: "center", behavior: "smooth"})}
+    else {
+      section.scrollIntoView({block: "start", behavior: "smooth"})}
+    });
+    /* appending list item to unordered list */
+    navUl.appendChild(node);
   }
-  else {
-    myNav.replaceChild(myNavSmall, myNavLarge)
+    /* appending unordered list to nav element in HTML file */
+    myNav.appendChild(navUl);
+
+    underlineExtend();
   }
-}
 
-/* Creating a media query for large screen sizes */
-const w = window.matchMedia("(min-width: 550px)")
+/* ===================================
 
-/* Adding an event listener to the window */
-w.addListener(mediaWidth);
+Adding additional sections
 
-/* Calling listener function at run time */
-mediaWidth(w);
+=================================== */
 
-/* Testing functionality by creating 20 additional sections */
+/* Testing functionality by creating additional sections */
+/* This will be prompted by a user-defined input number */
 
 function addSections() {
 
-  const myContent = "<h2><span>Section 1</span></h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p><p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>"
+  /* section content will be added all at once using innerHTML property */
+  const myContent = "<h2><span>Section 1</span></h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p><p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>";
 
-  for (let i = 5; i<=25; i++) {
+  let userInput = window.prompt("Hello there, how many additional sections would you like to add?");
+
+  /* starting at section 5 because we already have sections 1-4 in our HTML
+  file */
+  for (let i = 5; i<=userInput; i++) {
     const newElement = document.createElement("section");
     const sectionNum = `Section-${i}`;
     const myNumberedContent = myContent.replace("Section 1", sectionNum);
-    newElement.innerHTML = myNumberedContent
+    newElement.innerHTML = myNumberedContent;
+    /* adding data attribute for our navInit function */
     newElement.setAttribute("data-nav", sectionNum);
-    const main = document.querySelector("main")
+    const main = document.querySelector("main");
+    /* appending new sections at end of main section of HTML file */
     main.appendChild(newElement);
   }
 }
@@ -185,21 +107,131 @@ section headers
 /* Each h2 container contains an inline span element that triggers the underline */
 /* The reason for this is because I want to only trigger the expanded underline when
 the user hovers over the text (contained within the span) rather than the entire header
-which stretches across full width of the container */
+which stretches across full width of the container. If it was just a hover effect on then
+h2 this could have been resolved in css */
 
-const mySpans = document.getElementsByTagName("span");
+function underlineExtend () {
 
-for (let span of mySpans) {
-  /* Storing the original value of the underline length in order to revert for mouseout */
-  myElement = getComputedStyle(span.parentElement);
-  const storedElem = myElement.getPropertyValue("background-size");
-  /* Update value of the underline based on mouseover */
-  span.onmouseover = function() {
-    span.parentElement.classList.add("underline-extend")
+  const mySpans = document.getElementsByTagName("span");
+
+  for (let span of mySpans) {
+    span.onmouseover = function() {
+      /* targeting the parent h2 heading to increase length of underline */
+      span.parentElement.classList.add("underline-extend")
+    };
+    span.onmouseout = function() {
+      span.parentElement.classList.remove("underline-extend")
+    };
   }
-  /* Revert to original underline size with mouseout */
-  span.onmouseout = function() {
-    myElement = getComputedStyle(span.parentElement);
-    span.parentElement.style.backgroundSize = storedElem;
+}
+
+/* ===================================
+
+Adding Active Class marker to sections in
+viewport
+
+=================================== */
+
+/* defining function to move the active-class id to section in viewport
+This function will be activated by an event listener defined below */
+
+function activeClass () {
+
+  /* grabbing our sections and our active class id hook */
+    const mySections = document.getElementsByTagName("section");
+    myClass = document.querySelector("#active-class");
+    /* looping through our sections */
+    for (let section of mySections) {
+      /* get dimensions of our section */
+      const dimensions = section.getBoundingClientRect();
+      /* on large screens we want our entire section to be in the viewport to
+      be classified as active */
+      if (dimensions.top >=0 && dimensions.bottom <= (window.innerHeight
+        || document.documentElement.clientHeight) && window.innerWidth > 400){
+          myClass.removeAttribute("id");
+          section.setAttribute("id", "active-class");
+        }
+      /* on small screens our section is active if the top is within viewport */
+      else if (window.innerWidth <= 400 && dimensions.top >=0 &&
+      dimensions.top < window.innerHeight) {
+        myClass.removeAttribute("id");
+        section.setAttribute("id", "active-class");
+      }
+      else {
+        continue;
+      }
+    }
   }
+
+
+/* Creating an event listener for when scrolling stops
+This creates a smoother transition of active-class while scrolling through
+the page */
+
+  function activeClassScrollStop () {
+    /* Initializing timer */
+    let timer = null;
+
+    window.addEventListener("scroll", () => {
+      /* If we scroll while our timer is running, then reset timer */
+      if (timer !== null) {
+        clearTimeout(timer);
+      }
+      /* Set a timeout to fire for 200ms after we started scrolling
+      Only for large screen sizes. For small screen sizes due to
+      increased speed of scroll we switch active class immediately, also
+      to prevent an "overscroll" where user scrolls to middle of p element,
+      therefore jumping over section top active class trigger  */
+
+      if (window.innerWidth <= 550) {
+        activeClass();
+      }
+      else {
+        timer = setTimeout(() => {
+          activeClass();
+        }, 200);
+      }
+    });
+  }
+
+
+/* ===================================
+
+Adding scroll to top functionality to
+sticky chevron
+
+=================================== */
+
+function chevronScrollToTop () {
+
+  const pageTop = document.querySelector("body");
+
+  const sticky = document.querySelector(".sticky-icky-hover-helper-container");
+
+  sticky.addEventListener("click", () => {
+    pageTop.scrollIntoView({block: "start", behavior: "smooth"})
+  });
+}
+
+/* adding a function to rotate chevron counterclockwise by 90° when
+scrolling down */
+
+function rotateChevron () {
+
+  const chevron = document.querySelector(".sticky-icky__chevron");
+
+  window.addEventListener("scroll", () => {
+    /* opting against using an IntersectionObserver because this would also
+    trigger the rotate when the chevron enters the bottom of the viewport.
+    I only want a trigger when the chevron enters the top area i.e. when it
+    sticks to the top */
+    let dimensions = chevron.getBoundingClientRect();
+    /* setting trigger of 50px to top of viewport */
+    if (dimensions.top < 50) {
+      chevron.classList.add("rotate")
+    }
+    else {
+      chevron.classList.remove("rotate")
+    }
+  });
 }
