4b2a02da636ba01d0b7b892607d10c28bd62c6ed
Tino Leitzmann <cmleitzmann@gmail.com>
2021-08-17 10:57:42 +0200

    initializing README, html, css, package.JSON and webpack config scripts

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -1,23 +1,45 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+      libraryTarget: "var",
+      library: "Client",
+      // this eliminates the need for the clean webpack plugin
+      clean: true,
+    },
     mode: 'production',
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
             }
         ]
     },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+      optimization: {
+        minimizer: [
+          new TerserPlugin(),new CssMinimizerPlugin()
+        ],
+      },
+      plugins: [
+          new HtmlWebPackPlugin({
+              template: "./client/index.html",
+              filename: "./index.html",
+          }),
+          new MiniCssExtractPlugin({
+              filename: "[name].css"
+          })
+      ]
+    };
