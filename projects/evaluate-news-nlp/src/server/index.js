de66f1051170a805ef3fc6fa30d126444a045aff
Tino Leitzmann <cmleitzmann@gmail.com>
2021-09-01 16:30:19 +0200

    setting prod server different to dev server

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 
 // checking if a port is available in the environment variable
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 // designates what port the app will listen to for incoming requests
 app.listen(port, function () {
